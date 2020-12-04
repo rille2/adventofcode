d10c0fec614da66b35d9619f373bdac7cba0c628
rille2 <jakob.o.ristner@gmail.com>
2020-12-04 14:35:26 +0000

    soon done

--- passport/partone/main.hs
+++ passport/partone/main.hs
@@ -3,14 +3,11 @@ import Data.Char
 
 getPassports :: IO [[(String, String)]]
 getPassports = do
-    p <- readFile "false"
+    p <- readFile "passports"
     return $ map prune $ map words (splitOn "\n\n" p)
         where prune [] = []
               prune (c:cs) = (splut !! 0, splut !! 1) : prune cs
                   where splut = splitOn ":" c
-
--- reverse $ take 2 $ reverse string cm in
--- reverse $ drop 2 $ reverse strign num
 
 validPair :: (String, String) -> Bool
 validPair ("byr", yr) = length yr == 4 && ryr >= 1920 && ryr <= 2002 
@@ -26,13 +23,17 @@ validPair ("hgt", hg) = (afx == "in" && lng >= 59 && lng <= 76) || (afx == "cm" 
     where afx = reverse $ take 2 $ reverse hg 
           lng = read $ reverse $ drop 2 $ reverse hg
 
-validPair ("hcl", hc) = head hc == '#' && length hc == 7 && and [isNumber c || isLetter c | c <- drop 1 hc]
+validPair ("hcl", hc) = head hc == '#' && length hc == 7 && (and [isLetterAf c | c <- drop 2 hc])
+    where isLetterAf c = c `elem` ['a', 'b', 'c', 'd', 'e', 'f', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
-validPair ("ecl", ec) = sum (map toInt [ec == s | s <- ["amb", "blu", "brn","gry", "grn", "hzl", "oth"]]) == 1
+validPair ("ecl", ec) = ec `elem` ["amb", "blu", "brn","gry", "grn", "hzl", "oth"]
 
 validPair ("pid", p) = length p == 9 && and [isNumber x | x <- p]
 
-validPair ("cid", _ ) = True
+validPair _ = True
+
+
+
 
 toInt :: Bool -> Int
 toInt True = 1
