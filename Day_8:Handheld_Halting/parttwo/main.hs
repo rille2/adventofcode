bb83f484e80a8e16b5e12a88634113d07d4b1039
rille2 <jakob.o.ristner@gmail.com>
2020-12-08 10:43:21 +0000

    finished day 8

--- Day_8:Handheld_Halting/parttwo/main.hs
+++ Day_8:Handheld_Halting/parttwo/main.hs
@@ -6,18 +6,31 @@ parse (c:cs) = (take 3 c, val $ drop 4 c) : parse cs
             | otherwise = read str
 
 
+allparse :: [(String, Int)] -> Int -> [[(String, Int)]]
+allparse parsed ind
+  | ind >= length parsed = []
+  | fst p == "jmp" || fst p == "nop" = ((take (ind) parsed) ++ (opposite (fst p), snd p):(drop (ind + 1) parsed)) : (allparse parsed (ind + 1))
+  | otherwise = allparse parsed (ind + 1)
+        where p = parsed !! ind
+
+opposite :: String -> String
+opposite "jmp" = "nop"
+opposite "nop" = "jmp"
+
+
 main :: IO ()
 main = do
     p <- readFile "ops"
-    print $ length (parse $ lines p)
-    print $ traverseOps (head $ parse $ lines p) (parse $ lines p) [] 0 0
+    print $ head $ concat [traverseOps (head x) x [] 0 0 | x <- (allparse (parse (lines p)) 0)] 
 
-traverseOps :: (String, Int) -> [(String, Int)] -> [Int] -> Int -> Int -> Int
+traverseOps :: (String, Int) -> [(String, Int)] -> [Int] -> Int -> Int -> [Int]
 traverseOps op allops traversed index accm
-  | fst op == "nop" && ((index + 1) `elem` traversed) = 1
-  | fst op == "acc" && ((index + 1) `elem` traversed) = 1
-  | fst op == "jmp" && ((index + snd op) `elem` traversed) = 1
+  | fst op == "nop" && ((index + 1) `elem` traversed) = []
+  | fst op == "acc" && ((index + 1) `elem` traversed) = []
+  | fst op == "jmp" && ((index + snd op) `elem` traversed) = []
+  | fst op == "nop" && (index + 1) >= length allops = [accm]
+  | fst op == "acc" && (index + 1) >= length allops = [accm + snd op]
+  | fst op == "jmp" && (index + snd op) >= length allops = [accm]
   | fst op == "nop" = traverseOps (allops !! (index + 1)) allops (index:traversed) (index + 1) accm
   | fst op == "acc" = traverseOps (allops !! (index + 1)) allops (index:traversed) (index + 1) (accm + snd op)
   | fst op == "jmp" = traverseOps (allops !! (index + snd op)) allops (index:traversed) (index + snd op) accm
-  | otherwise = accm
