06d564103113529b4b89d2dda178b5921bfe6889
rille2 <jakob.o.ristner@gmail.com>
2020-12-09 19:18:20 +0000

    now lighning fast with binary search

--- Day_9:Encoding_Error/parttwo/main.hs
+++ Day_9:Encoding_Error/parttwo/main.hs
@@ -1,10 +1,23 @@
-findSeq :: [Int] -> Int -> Int -> Int
-findSeq (x:xs) currsl sc
-  | sum seq == sc = minimum seq + maximum seq
-  | sum seq > sc = findSeq xs 1 sc
-  | otherwise = findSeq (x:xs) (currsl + 1) sc
-    where seq = (x : (take currsl xs))
+import Prelude hiding (sequence)
+
+sequence :: [Int] -> Int -> [Int]
+sequence xs x = take x xs
+
+search :: [Int] -> Int -> Int -> Int -> [(Int, Int)] -> Int
+search xs lo hi x tried
+  | (lo, hi) `elem` tried = -1
+  | sum seq == x = minimum seq + maximum seq
+  | sum seq > x = search xs lo mid x ((lo, hi):(take 1 tried))
+  | sum seq < x = search xs mid hi x ((lo, hi):(take 1 tried))
+    where mid = (lo + hi) `div` 2
+          seq = sequence xs mid
+ 
+find :: [Int] -> Int -> Int
+find (x:xs) y
+  | s == -1 = find xs y
+  | otherwise = s
+    where s = search (x:xs) 0 (length xs) y []
 
 main = do
     f <- readFile "input"
-    print $ findSeq [read x | x <- lines f] 1 3199139634
+    print $ find [read x | x <- lines f] 3199139634
