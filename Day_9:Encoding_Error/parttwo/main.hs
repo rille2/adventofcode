4c46a5cf64a89bb20c1bb1fbc1460b350a61b15d
rille2 <jakob.o.ristner@gmail.com>
2020-12-09 13:15:14 +0000

    improved again

--- Day_9:Encoding_Error/parttwo/main.hs
+++ Day_9:Encoding_Error/parttwo/main.hs
@@ -2,7 +2,6 @@ findSeq :: [Int] -> Int -> Int -> Int
 findSeq (x:xs) currsl sc
   | sum seq == sc = minimum seq + maximum seq
   | sum seq > sc = findSeq xs 1 sc
-  | currsl == length xs = findSeq xs 1 sc
   | otherwise = findSeq (x:xs) (currsl + 1) sc
     where seq = (x : (take currsl xs))
 
